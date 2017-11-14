c6f61a023b5971ae8869ca4e8e0fd4dfec42cc57
Alex de Mulder <alex@almende.org>
2017-11-14 14:46:37 +0100

    added pointersystem to hydrate method.

--- js/router/store/hydrator.ts
+++ js/router/store/hydrator.ts
@@ -3,9 +3,16 @@ import { AsyncStorage } from 'react-native'
 function hydrate(userId) {
   // get all keys
   let data = {};
+  let pointerIndex = {};
+
+  function storePointer(pointer, key) {
+    pointerIndex[key] = pointer;
+  }
+
   AsyncStorage.getAllKeys()
     .then((keys) => {
       let userKeys = [];
+      let keyListForRetreival = [];
       for (let i = 0; i < keys.length; i++) {
         let key = keys[i];
         let keyArray = key.split('.');
@@ -17,19 +24,39 @@ function hydrate(userId) {
       userKeys.sort((a,b) => { return a.key.length - b.key.length });
 
       for (let i = 0; i < userKeys.length; i++) {
+        let key = userKeys[i].key;
         let keyArray = userKeys[i].arr
 
         // index 0 is the userId
         let pointer = data;
         for (let j = 1; j < keyArray; j++) {
           if (keyArray.length > j+1) {
-            pointer[keyArray[j]] = {};
+            if (pointer[keyArray[j]] === undefined) {
+              pointer[keyArray[j]] = {};
+            }
             pointer = pointer[keyArray[j]]
           }
+          else {
+            // end of path
+            // get the data on this key
+            storePointer(pointer, key);
+          }
+        }
+
+        keyListForRetreival.push(key);
+      }
+      console.log('dataTree:', data);
+      return AsyncStorage.multiGet(keyListForRetreival)
+    })
+    .then((keyValuePairArray) => {
+      for (let i = 0; i < keyValuePairArray.length; i++) {
+        let pair = keyValuePairArray[i];
+        if (pointerIndex[pair[0]]) {
+          pointerIndex[pair[0]] = pair[1];
         }
       }
 
-      console.log('dataTree:', data);
+      console.log("all data", data);
     })
     .catch((err) => { console.log('failed to get all keys'); })
   // construct tree
