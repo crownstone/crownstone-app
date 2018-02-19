508c260f45f791bf9d095aba64c564fbb829c22c
Alex de Mulder <alexdemulder@gmail.com>
2018-02-19 14:46:01 +0100

    expanded ble debug

--- js/ExternalConfig.ts
+++ js/ExternalConfig.ts
@@ -6,7 +6,7 @@ const DeviceInfo = require('react-native-device-info');
 
 
     // ONLY CHANGE THIS LINE IF YOU WANT TO DISABLE RELEASE MODE
-    const RELEASE_MODE = true;
+    const RELEASE_MODE = false;
 
     // IF TRUE, USED TO FAKE RELEASE MODE BUT WITH DEBUGGING
     const IGNORE_LOCAL_CONFIG = false;
