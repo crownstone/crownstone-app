a1d42a8c335d33f7dd9ea74c6c5f4b3edb7a4701
vliedel <bart@crownstone.rocks>
2020-04-30 14:34:46 +0200

    [android] Updates for new version react native.

--- android/app/src/main/java/rocks/crownstone/consumerapp/BluenetBridgePacket.java
+++ android/app/src/main/java/rocks/crownstone/consumerapp/BluenetBridgePacket.java
@@ -8,7 +8,6 @@
 package rocks.crownstone.consumerapp;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -17,23 +16,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Copyright (c) 2015 Bart van Vliet <bart@dobots.nl>. All rights reserved.
- * <p/>
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3, as
- * published by the Free Software Foundation.
- * <p/>
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 3 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- * <p/>
- * Created on 22-9-16
- *
- * @author Bart van Vliet
- */
 public class BluenetBridgePacket implements ReactPackage {
 	private static BluenetBridge bluenetBridge = null;
 
--- android/app/src/main/java/rocks/crownstone/consumerapp/MainApplication.java
+++ android/app/src/main/java/rocks/crownstone/consumerapp/MainApplication.java
@@ -1,36 +1,12 @@
 package rocks.crownstone.consumerapp;
 
-import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
-
-import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.cmcewen.blurview.BlurViewPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.reactnativecommunity.slider.ReactSliderPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.horcrux.svg.SvgPackage;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.corbt.keepawake.KCKeepAwakePackage;
-import fr.bamlab.rnimageresizer.ImageResizerPackage;
-import com.rnfs.RNFSPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-//import com.facebook.react.modules.storage.ReactDatabaseSupplier;
-//import com.facebook.react.ReactNativeHost;
-//import com.facebook.react.ReactPackage;
-//import com.facebook.react.shell.MainReactPackage;
-//import com.facebook.soloader.SoLoader;
-//import com.reactnativenavigation.react.ReactGateway;
-//import com.reactnativenavigation.react.SyncUiImplementation;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -51,6 +27,7 @@ public class MainApplication extends NavigationApplication {
 					List<ReactPackage> packages = new PackageList(this).getPackages();
 					// Packages that cannot be autolinked yet can be added manually here, for example:
 					// packages.add(new MyReactNativePackage());
+					packages.add(new BluenetBridgePacket());
 					return packages;
 				}
 
@@ -98,66 +75,3 @@ public class MainApplication extends NavigationApplication {
 		}
 	}
 }
-
-//public class MainApplication extends NavigationApplication {
-//
-//	@Override
-//	protected ReactGateway createReactGateway() {
-//		ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
-//			@Override
-//			protected String getJSMainModuleName() {
-//				return "index";
-//			}
-//
-////			@Override
-////			protected UIImplementationProvider getUIImplementationProvider() {
-////				return new SyncUiImplementation.Provider();
-////			}
-//		};
-//		return new ReactGateway(this, isDebug(), host);
-//	}
-//
-//	@Override
-//	public boolean isDebug() {
-//		return BuildConfig.DEBUG;
-//	}
-//
-//	protected List<ReactPackage> getPackages() {
-//		// Add additional packages you require here
-//		// No need to add RnnPackage and MainReactPackage
-//		return Arrays.<ReactPackage>asList(
-//				new BluenetBridgePacket(),
-//				new MainReactPackage(),
-//				new RNGestureHandlerPackage(),
-//				new ReanimatedPackage(),
-//                new ExtraDimensionsPackage(),
-//            	new BlurViewPackage(),
-//				new RNCameraPackage(),
-//				new AsyncStoragePackage(),
-//				new ReactSliderPackage(),
-//				new VectorIconsPackage(),
-//				new SvgPackage(),
-//				new ImageResizerPackage(),
-//				new RNFSPackage(),
-//				new KCKeepAwakePackage(),
-//				new SplashScreenReactPackage(),
-//				new RNSentryPackage(),
-//				new RNDeviceInfo(),
-//				new ReactNativePushNotificationPackage(),
-//				new MapsPackage()
-//		);
-//	}
-//
-//	@Override
-//	public List<ReactPackage> createAdditionalReactPackages() {
-//		return getPackages();
-//	}
-//
-//	@Override
-//	public void onCreate() {
-//		super.onCreate();
-////		long size = 50L * 1024L * 1024L; // 50 MB
-////		ReactDatabaseSupplier.getInstance(getApplicationContext()).setMaximumSize(size);
-////
-//	}
-//}
