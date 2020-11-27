9e8ecdee6ca4369dd954b32d2f88cc90e840f295
vliedel <bart@crownstone.rocks>
2020-11-27 14:50:25 +0100

    [android] Implemented hub command and service data.

---
+++ android/app/src/main/java/rocks/crownstone/consumerapp/HubData.kt
@@ -0,0 +1,41 @@
+package rocks.crownstone.consumerapp
+
+import nl.komponents.kovenant.Promise
+import nl.komponents.kovenant.deferred
+import nl.komponents.kovenant.then
+import rocks.crownstone.bluenet.Bluenet
+import rocks.crownstone.bluenet.packets.HubDataPacket
+import rocks.crownstone.bluenet.structs.Errors
+import rocks.crownstone.bluenet.util.Conversion
+import rocks.crownstone.consumerapp.hubdata.HubDataReplyPacket
+import rocks.crownstone.consumerapp.hubdata.HubDataRequestPacket
+import rocks.crownstone.consumerapp.hubdata.request.TokenAndCloudIdPacket
+
+class HubData(bluenet: Bluenet) {
+	private val bluenet = bluenet
+
+	fun sendTokenAndCloudId(hubToken: String, cloudId: String): Promise<HubDataReplyPacket, Exception> {
+		val reqPacket = HubDataRequestPacket(HubDataRequestPacket.HubDataRequestType.SETUP, TokenAndCloudIdPacket(hubToken, cloudId))
+		return sendHubDataCommand(reqPacket)
+	}
+
+	internal fun sendHubDataCommand(reqPacket: HubDataRequestPacket): Promise<HubDataReplyPacket, Exception> {
+		val deferred = deferred<HubDataReplyPacket, Exception>()
+		bluenet.control.hubData(HubDataPacket(HubDataPacket.EncryptType.NOT_ENCRYPTED, reqPacket))
+				.then {
+					// TODO: maybe control.hubData() should return HubDataReplyPacket ?
+					val replyPacket = HubDataReplyPacket()
+					val array = it.getArray()
+					if (array == null) {
+						deferred.reject(Errors.SizeWrong())
+						return@then
+					}
+					if (!replyPacket.fromArray(array)) {
+						deferred.reject(Errors.Parse("Can't make a hub data reply packet from ${Conversion.bytesToString(array)}"))
+						return@then
+					}
+					deferred.resolve(replyPacket)
+				}
+		return deferred.promise
+	}
+}
---
+++ android/app/src/main/java/rocks/crownstone/consumerapp/hubdata/HubDataRequestPacket.kt
@@ -0,0 +1,61 @@
+/**
+ * Author: Crownstone Team
+ * Copyright: Crownstone (https://crownstone.rocks)
+ * Date: Nov 26, 2020
+ * License: LGPLv3+, Apache License 2.0, and/or MIT (triple-licensed)
+ */
+
+package rocks.crownstone.consumerapp.hubdata
+
+import rocks.crownstone.bluenet.packets.PacketInterface
+import rocks.crownstone.bluenet.packets.wrappers.PayloadWrapperPacket
+import rocks.crownstone.bluenet.structs.Uint16
+import rocks.crownstone.bluenet.structs.Uint8
+import rocks.crownstone.bluenet.util.putUint16
+import rocks.crownstone.bluenet.util.putUint8
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+class HubDataRequestPacket(type: HubDataRequestType, payload: PacketInterface?): PayloadWrapperPacket(payload) {
+	override val TAG = this.javaClass.simpleName
+	companion object {
+		const val HEADER_SIZE = 1 + 2
+	}
+
+	val protocol: Uint8 = 0U
+	val type = type
+
+	enum class HubDataRequestType(val num: Uint16) {
+		SETUP(0U),
+		UNKNOWN(0xFFFFU);
+		companion object {
+			private val map = values().associateBy(HubDataRequestType::num)
+			fun fromNum(type: Uint16): HubDataRequestType {
+				return map[type] ?: return UNKNOWN
+			}
+		}
+	}
+
+	override fun getHeaderSize(): Int {
+		return HEADER_SIZE
+	}
+
+	override fun getPayloadSize(): Int? {
+		return null
+	}
+
+	override fun headerToBuffer(bb: ByteBuffer): Boolean {
+		bb.order(ByteOrder.LITTLE_ENDIAN)
+		bb.putUint8(protocol)
+		bb.putUint16(type.num)
+		return true
+	}
+
+	override fun headerFromBuffer(bb: ByteBuffer): Boolean {
+		return false
+	}
+
+	override fun toString(): String {
+		return "HubDataRequestPacket(protocol=$protocol, type=$type, payload=$payload)"
+	}
+}
