e1966b12a32b9868478977eabf8553972685d23b
vliedel <bart@crownstone.rocks>
2022-01-20 11:56:44 +0100

    Optionally provide ip address to ui test script.

--- run_UI_tests_android.sh
+++ run_UI_tests_android.sh
@@ -1,12 +1,26 @@
 # Set to dir with the cloud test container repo.
 CLOUD_DIR="../cloud-test-container"
 
-export IP_ADDRESS=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+usage () {
+	echo "Usage: $0 [local_IP_address]"
+	exit 1
+}
 
-# Set to your IP address, or leave commented out to use the result of ifconfig.
-#export IP_ADDRESS="1.2.3.4"
+if [ $# -gt 0 ]; then
+	export IP_ADDRESS="$1"
+else
+	export IP_ADDRESS=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+fi
+
+if [ `echo "$IP_ADDRESS" | wc -l` -gt 1 ]; then
+	echo "There are multiple matches for your IP address, please provide your IP address as argument."
+	usage
+fi
+
+echo "Using $IP_ADDRESS as local IP address." 
 
 ${CLOUD_DIR}/reset_mongo_db.sh
 
-detox test --configuration android-debug-device
+detox build --configuration android-debug-device
+detox test --configuration android-debug-device -l verbose
 
--- run_UI_tests_ios.sh
+++ run_UI_tests_ios.sh
@@ -1,6 +1,25 @@
-SCRIPT_DIR="../cloud-test-container"
-export IP_ADDRESS=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+# Set to dir with the cloud test container repo.
+CLOUD_DIR="../cloud-test-container"
 
-${SCRIPT_DIR}/reset_mongo_db.sh
+usage () {
+	echo "Usage: $0 [local_IP_address]"
+	exit 1
+}
+
+if [ $# -gt 0 ]; then
+	export IP_ADDRESS="$1"
+else
+	export IP_ADDRESS=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+fi
+
+if [ `echo "$IP_ADDRESS" | wc -l` -gt 1 ]; then
+	echo "There are multiple matches for your IP address, please provide your IP address as argument."
+	usage
+fi
+
+echo "Using $IP_ADDRESS as local IP address." 
+
+${CLOUD_DIR}/reset_mongo_db.sh
 
 detox test --configuration ios-debug
+
