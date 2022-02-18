8f500fb96210fb97cf9cb9316f0dc58897f3a7ec
vliedel <bart@crownstone.rocks>
2022-02-18 14:02:34 +0100

    [android] fixed batter stats server script.

--- android/battery-stats-server.sh
+++ android/battery-stats-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting server, browse to http://localhost:5678/"
-docker --run -p 5678:9999 gcr.io/android-battery-historian/stable:3.0 --port 9999
+docker run -p 5678:9999 gcr.io/android-battery-historian/stable:3.0 --port 9999
