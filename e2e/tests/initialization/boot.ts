919de46dc531f72da50d8e8c299d227b0136348d
vliedel <bart@crownstone.rocks>
2022-03-01 15:39:23 +0100

    Fixed UI test startup.

--- e2e/tests/initialization/boot.ts
+++ e2e/tests/initialization/boot.ts
@@ -4,5 +4,6 @@ export const BootApp = () => {
   test("should boot the app", async () => {
     let launchArguments = device.appLaunchArgs.get();
     await device.launchApp({launchArgs: { detoxEnableSynchronization: 0, ...launchArguments }});
+    await device.disableSynchronization();
   });
 }
