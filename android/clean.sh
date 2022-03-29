9b9123eafb236b4006298425d0dfb1bec3812de3
vliedel <bart@crownstone.rocks>
2022-03-29 16:06:20 +0200

    Show how to get rid of RN debug popups.

--- android/clean.sh
+++ android/clean.sh
@@ -2,4 +2,5 @@
 rm -r .idea
 rm -r .gradle
 rm Crownstone.iml
-
+rm android-Crownstone.iml
+rm app/android-app.iml
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ LogBox.ignoreLogs([
   /.*/,
 ])
 
+// Uncomment when you don't want the RN debug popups in app
+//LogBox.ignoreAllLogs()
+
 import { USE_ERROR_REPORTING } from "./app/js/ExternalConfig";
 import { loadRoutes } from "./app/js/views/Routes";
 import { BackgroundProcessHandler } from "./app/js/backgroundProcesses/BackgroundProcessHandler";
